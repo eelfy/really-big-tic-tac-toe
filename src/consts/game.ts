9c6a4a5d3ae20166c9dc4518e4cdca0d035a4462
eelfy <eeflyza@gmail.com>
2022-10-05 13:38:26 +0300

    add decrease game field size

--- src/consts/game.ts
+++ src/consts/game.ts
@@ -2,8 +2,8 @@ import { validateMatchesToWin } from '../utils';
 
 const C = process.env.MATCHES_TO_WIN!;
 
-const MAP_ROWS = 50;
-const MAP_COLUMNS = 50;
+const MAP_ROWS = 30;
+const MAP_COLUMNS = 30;
 const MATCHES_TO_WIN = validateMatchesToWin(C);
 
 export { MAP_ROWS, MAP_COLUMNS, MATCHES_TO_WIN };
